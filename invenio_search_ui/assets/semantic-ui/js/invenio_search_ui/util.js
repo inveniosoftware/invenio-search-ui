1af3496f742b3059f996102a05668e59fbcb2116
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2022-04-05 10:34:57 +0200

    search app: add optional wrapper to root element

--- invenio_search_ui/assets/semantic-ui/js/invenio_search_ui/util.js
+++ invenio_search_ui/assets/semantic-ui/js/invenio_search_ui/util.js
@@ -27,19 +27,23 @@ export function createSearchAppInit(
   defaultComponents,
   autoInit = true,
   autoInitDataAttr = "invenio-search-config",
-  multi = false
+  multi = false,
+  ContainerComponent = React.Fragment,
 ) {
+
   const initSearchApp = (rootElement) => {
     const { appId, ...config } = JSON.parse(
       rootElement.dataset[_camelCase(autoInitDataAttr)]
     );
     loadComponents(appId, defaultComponents).then((res) => {
       ReactDOM.render(
-        <SearchApp
-          config={config}
-          // Use appName to namespace application components when overriding
-          {...(multi && { appName: appId })}
-        />,
+        <ContainerComponent>
+          <SearchApp
+            config={config}
+            // Use appName to namespace application components when overriding
+            {...(multi && { appName: appId })}
+          />
+        </ContainerComponent>,
         rootElement
       );
     });
