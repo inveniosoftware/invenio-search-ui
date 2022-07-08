636c35617e8c20414afb0e7f05ab7ae43825c9f5
Nicola Tarocco <ntarocco@gmail.com>
2022-07-08 17:03:11 +0200

    js: remove app-rdm dependency

--- invenio_search_ui/assets/semantic-ui/js/invenio_search_ui/components/Results.js
+++ invenio_search_ui/assets/semantic-ui/js/invenio_search_ui/components/Results.js
@@ -21,7 +21,6 @@ import {
 } from "react-searchkit";
 import { Grid } from "semantic-ui-react";
 import { SearchConfigurationContext } from "./context";
-import { i18next } from "@translations/invenio_app_rdm/i18next";
 
 export const Results = ({ currentResultsState = {} }) => {
   const { total } = currentResultsState.data;
@@ -111,7 +110,6 @@ export const ResultOptions = ({ currentResultsState = {} }) => {
                   <Sort
                     sortOrderDisabled={sortOrderDisabled || false}
                     values={sortOptions}
-                    ariaLabel={i18next.t("Sort")}
                   />
                 </Overridable>
               )}
